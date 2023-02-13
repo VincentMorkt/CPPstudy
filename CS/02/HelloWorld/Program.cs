08f24cddf0caad7af5b13001d015561b27187fe0
Vincent Morkt <cmtwyofupnl@gmail.com>
2023-02-13 14:19:21 +0200

    move out all non-cpp stuff in master

--- CS/02/HelloWorld/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using  System;
-SayHello();
-void SayHello()
-{
-    Console.WriteLine("Hello, world");
-    Console.WriteLine("ололо ось вона ось вона риба моєї мрії");
-}
